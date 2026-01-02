11875e1ec937660e0615edc8c2b14f3ae25236ea
bronkstar <bjoern.t.richter@googlemail.com>
2026-01-02 14:48:49 +0100

    Opt in to Material3 TopAppBar API

--- app/src/main/java/com/wasessenwir/app/MainActivity.kt
+++ app/src/main/java/com/wasessenwir/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -26,6 +27,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 private fun WasEssenWirApp() {
     Scaffold(
         topBar = {
