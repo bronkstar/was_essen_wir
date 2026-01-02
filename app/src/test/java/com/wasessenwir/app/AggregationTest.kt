7ad46fe93140adfc5dbba53d85bac486c000e3c7
bronkstar <bjoern.t.richter@googlemail.com>
2026-01-02 15:11:05 +0100

    Add unit test scaffold

---
+++ app/src/test/java/com/wasessenwir/app/AggregationTest.kt
@@ -0,0 +1,11 @@
+package com.wasessenwir.app
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AggregationTest {
+    @Test
+    fun placeholder_sanity() {
+        assertEquals(4, 2 + 2)
+    }
+}
